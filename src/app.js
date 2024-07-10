ad4216e9f2a138596ed6a41d65c32c07a4df00fd
Rehan <rehanalire52@gmail.com>
2024-07-10 19:02:20 +0530

    Restructure backend folder structure for production readiness

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors'
+
+import postJob from './routes/Job.routs.js'
+import authRoute from './routes/Auth.routs.js'
+import profileRoute from './routes/Profile.routs.js'
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use('/postjob', postJob);
+app.use('/auth', authRoute);
+app.use('/profile', profileRoute);
+
+export default app
---
+++ src/server.js
@@ -0,0 +1,16 @@
+import env from 'dotenv';
+import app from "./app.js";
+import dbConnect from './db/dbConnect.js'
+
+env.config();
+
+// database connection
+dbConnect()
+.then(() => {
+    app.listen(process.env.PORT || 1000, () => {
+        console.log(`Server is running at port ${process.env.PORT}...`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed", err);
+})
