515b5331008253017f7f3f4b92039e22cce0468e
Rehan <rehanalire52@gmail.com>
2024-07-10 19:17:24 +0530

    add express-rate-limit middleware to code base

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 import postJob from './routes/Job.routs.js';
 import authRoute from './routes/Auth.routs.js';
@@ -7,13 +8,23 @@ import profileRoute from './routes/Profile.routs.js';
 
 const app = express();
 
+// Rate limiting configuration
+const limit = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again after 15 minutes'
+  });
+
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(limit());
+
 
 // routes
 app.use('/postjob', postJob);
 app.use('/auth', authRoute);
 app.use('/profile', profileRoute);
+
 
 export default app;
