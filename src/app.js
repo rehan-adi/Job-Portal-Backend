1167e6585e2b7907d5b70d0f6ef47fb3d512ea6f
Rehan <rehanalire52@gmail.com>
2024-07-11 13:23:37 +0530

    changes on auth.controllers.js for better error message

--- src/app.js
+++ src/app.js
@@ -8,32 +8,31 @@ import postJob from './routes/Job.routs.js';
 import authRoute from './routes/Auth.routs.js';
 import profileRoute from './routes/Profile.routs.js';
 
-
 const app = express();
 
 // Rate limiting configuration
 const limit = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-    message: 'Too many requests from this IP, please try again after 15 minutes'
-  });
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 15 minutes'
+});
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+  })
+);
 app.use(limit);
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(helmet());
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 // routes
-app.use('/postjob', postJob);
 app.use('/auth', authRoute);
+app.use('/postjob', postJob);
 app.use('/profile', profileRoute);
-
 
 export default app;
--- src/controllers/Auth.controllers.js
+++ src/controllers/Auth.controllers.js
@@ -2,42 +2,79 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
+
 export const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
-      return res.status(400).json({ message: 'Please fill all the fields' });
+      return res
+        .status(400)
+        .json({ success: false, message: 'Please fill all the fields' });
     }
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res
+        .status(400)
+        .json({ success: false, message: 'User already exists' });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
-    res.status(201).json({ message: 'User registered successfully' });
+    res
+      .status(201)
+      .json({
+        success: true,
+        user: user,
+        message: 'User registered successfully'
+      });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error(error);
+    res
+      .status(500)
+      .json({
+        success: true,
+        message: 'Failed to register',
+        error: error.message
+      });
   }
 };
+
 
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Please fill all the fields' });
+    }
     const user = await User.findOne({ email });
     if (!user) {
-      throw new Error('User not found');
+      return res
+        .status(400)
+        .json({ success: false, message: 'User not found' });
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      throw new Error('Invalid credentials, Password is incorrect');
+      return res.status(401).json({
+        success: false,
+        message: 'Incorrect password. Please try again.'
+      });
     }
     const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
       expiresIn: '1h'
     });
-    console.log('Login successful');
-    res.status(200).json({ token, message: 'Login successful' });
+    res
+      .status(200)
+      .json({ success: true, token, message: 'Login successfully' });
   } catch (error) {
-    res.status(401).json({ error: error.message });
+    console.error(error);
+    res
+      .status(401)
+      .json({
+        success: false,
+        message: 'Failed to login',
+        error: error.message
+      });
   }
 };
