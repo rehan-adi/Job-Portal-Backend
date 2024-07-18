271145a9c2de2d96c2729753dfb166a64387b96c
Rehan <rehanalire52@gmail.com>
2024-07-18 15:51:40 +0530

    create interface for User and use it in User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,13 @@
-import mongoose from 'mongoose';
+import { Schema, model } from 'mongoose';
+import { User } from '../interfaces/interfaces.js';
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema<User>({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true }
 });
 
-const userModel = mongoose.model('User', userSchema);
+const userModel = model<User>('User', userSchema);
 
 export default userModel;
+
