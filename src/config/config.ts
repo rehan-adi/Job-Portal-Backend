701e2a9ffaffd049d9ebc4853fc326834072b3f3
Rehan <rehanalire52@gmail.com>
2024-07-21 13:39:23 +0530

    add centralize environment variable configuration to code base

---
+++ src/config/config.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+interface Config {
+    PORT: number;
+    MONGODB_URI: string;
+    SECRET_KEY: string;
+    DB_NAME: string;
+    CORS_ORIGIN: string;
+}
+
+const config: Config = {
+    PORT: parseInt(process.env.PORT || '1000', 10),
+    MONGODB_URI: process.env.MONGODB_URI as string,
+    SECRET_KEY: process.env.SECRET_KEY as string,
+    DB_NAME: process.env.DB_NAME as string,
+    CORS_ORIGIN: process.env.CORS_ORIGIN as string,
+};
+
+export default config;
