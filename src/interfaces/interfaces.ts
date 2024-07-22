a1bf33e7ea66a5c723e2658fbf5b4f928f4b92d8
Rehan <rehanalire52@gmail.com>
2024-07-22 15:16:12 +0530

    update user interface

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,9 +1,9 @@
 import { Document } from 'mongoose';
 
 export interface User extends Document {
-    name: string;
     email: string;
     password: string;
+    role: string;
 }
 
 export interface Profile extends Document {
