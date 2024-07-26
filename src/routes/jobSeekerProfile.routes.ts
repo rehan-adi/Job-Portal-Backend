064cd1e376721d9a02ada48bc6f61c8b9d5c524c
Rehan <rehanalire52@gmail.com>
2024-07-26 23:12:27 +0530

    add jobseekerProfile update route to code base

--- src/routes/jobSeekerProfile.routes.ts
+++ src/routes/jobSeekerProfile.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
     jobSeekerProfileCreate,
     jobSeekerProfileGet,
+    jobSeekerProfileUpdate,
     jobSeekerProfileDelete
 } from '../controllers/jobSeekerProfile.controller.js';
 import { isLogin } from '../middlewares/isLogin.js';
@@ -10,6 +11,7 @@ const jobSeekerProfileRouter = Router();
 
 jobSeekerProfileRouter.post('/create', isLogin, jobSeekerProfileCreate);
 jobSeekerProfileRouter.get('/profile', isLogin, jobSeekerProfileGet);
+jobSeekerProfileRouter.put('/update', isLogin, jobSeekerProfileUpdate);
 jobSeekerProfileRouter.delete('/delete', isLogin, jobSeekerProfileDelete);
 
 export default jobSeekerProfileRouter;
