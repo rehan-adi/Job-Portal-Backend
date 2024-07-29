effd86ce66cc61df0c5322cb14ad4ae22eaa5d4c
Rehan <rehanalire52@gmail.com>
2024-07-29 13:40:32 +0530

    add employerProfileUpdate route to code base

--- src/routes/employerProfile.routes.ts
+++ src/routes/employerProfile.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { employerProfileCreate, employerProfileGet } from '../controllers/employerProfile.controller.js';
+import { employerProfileCreate, employerProfileGet, employerProfileUpdate } from '../controllers/employerProfile.controller.js';
 import { isEmployer, isLogin } from '../middlewares/isLogin.js';
 
 const employerProfileRouter = Router();
@@ -10,12 +10,17 @@ employerProfileRouter.post(
     isEmployer,
     employerProfileCreate
 );
-
 employerProfileRouter.get(
     '/profile',
     isLogin,
     isEmployer,
     employerProfileGet
+);
+employerProfileRouter.put(
+    '/update',
+    isLogin,
+    isEmployer,
+    employerProfileUpdate
 );
 
 export default employerProfileRouter;
