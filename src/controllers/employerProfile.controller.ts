d1f0871282b01acd88013ef3c48b5e756a499fc5
Rehan <rehanalire52@gmail.com>
2024-07-29 13:33:42 +0530

    add employerProfileGet controller to code base

--- src/controllers/employerProfile.controller.ts
+++ src/controllers/employerProfile.controller.ts
@@ -17,7 +17,7 @@ export const employerProfileCreate = async (
         if (!userId) {
             return res.status(400).json({
                 success: false,
-                message: 'User ID not found in request.'
+                message: 'User ID not found'
             });
         }
 
@@ -68,39 +68,39 @@ export const employerProfileCreate = async (
     }
 };
 
-// export const jobSeekerProfileGet = async (
-//     req: CustomRequest,
-//     res: Response
-// ) => {
-//     try {
-//         const userId = req.user?._id;
+export const employerProfileGet = async (
+    req: CustomRequest,
+    res: Response
+) => {
+    try {
+        const userId = req.user?.id;
 
-//         if (!userId) {
-//             return res.status(400).json({
-//                 success: false,
-//                 message: 'User ID not found in request.'
-//             });
-//         }
+        if (!userId) {
+            return res.status(400).json({
+                success: false,
+                message: 'User ID not found.'
+            });
+        }
 
-//         const profile = await jobSeekerModel.findOne({ user: userId });
+        const profile = await employerModel.findOne({ user: userId });
 
-//         if (!profile) {
-//             return res
-//                 .status(404)
-//                 .json({ success: false, message: 'Profile not found' });
-//         }
-//         return res
-//             .status(200)
-//             .json({ success: true, profile, message: 'Showing profile' });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({
-//             success: false,
-//             message: 'Failed to get Profile',
-//             error: error instanceof Error ? error.message : 'Unknown error'
-//         });
-//     }
-// };
+        if (!profile) {
+            return res
+                .status(404)
+                .json({ success: false, message: 'Profile not found' });
+        }
+        return res
+            .status(200)
+            .json({ success: true, profile, message: 'Showing profile' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            message: 'Failed to get Profile',
+            error: error instanceof Error ? error.message : 'Unknown error'
+        });
+    }
+};
 
 // export const jobSeekerProfileUpdate = async (
 //     req: CustomRequest,
